51e77352020e6d39793f893a85357572ad3df584
juliakocik <julia.kocik02@gmail.com>
2023-03-26 19:07:59 +0200

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cart from './components/routes/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Cart/>
     </div>
   );
 }
