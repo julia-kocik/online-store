39b72cce374ae4dab297ee450f3dbe96c7702679
juliakocik <julia.kocik02@gmail.com>
2023-04-10 20:45:16 +0200

    Add fetching categories with useeffect

--- src/components/features/Categories/Categories.tsx
+++ src/components/features/Categories/Categories.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Category = string
 
 const Categories = (): JSX.Element => {
+  const [categories, setCategories] = useState<Category[]>([])
+  useEffect(() => {
+    const fetchCategories = async (): Promise<void> => {
+      const response = await fetch('https://fakestoreapi.com/products')
+      const products = await response.json()
+      const categories = products.map(item => item.category)
+      setCategories(Array.from(new Set(categories)))
+    }
+    fetchCategories()
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div>
+      {categories.map(category => (
+        <div key={category}>
+          <h2>{category}</h2>
+        </div>
+      ))}
+    </div>
   )
 }
 
