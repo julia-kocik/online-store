3785dc1b050a522d97804294d2a0b92167653f9f
juliakocik <julia.kocik02@gmail.com>
2023-04-10 17:57:21 +0200

    Add Categories component and add it to Home page

---
+++ src/components/features/Categories/CategoriesStyles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+// import { colors } from '../../../colors'
+
+export const CategoriesContainer = styled.div`
+
+`
--- src/components/routes/Home/Home.tsx
+++ src/components/routes/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../features/Banner/Banner'
+import Categories from '../../features/Categories/Categories'
 
 const Home = (): JSX.Element => {
   return (
     <div>
       <Banner/>
+      <Categories/>
     </div>
   )
 }
