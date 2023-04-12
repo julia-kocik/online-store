f07a9890e7858434f77235ce54cb90b7de61b3ea
juliakocik <julia.kocik02@gmail.com>
2023-04-12 20:09:43 +0200

    Add Footer component

---
+++ src/components/features/Footer/FooterStyles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { colors } from '../../../colors'
+
+export const FooterContainer = styled.div`
+    background-color: ${colors.green};
+    position: fixed;
+    width: 100%;
+    height: 1rem;
+    left: 0;
+    bottom: 0;
+`
