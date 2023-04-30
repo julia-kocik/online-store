80dda6d8af3af28076c15a89650fc566c0bcb153
juliakocik <julia.kocik02@gmail.com>
2023-04-30 11:52:27 +0200

    Adjust Cart Counter to calculate totalCartAmount

--- src/components/features/CartIcon/CartIcon.tsx
+++ src/components/features/CartIcon/CartIcon.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsFillCartCheckFill } from 'react-icons/bs'
 import { CartContainer, CartCounter } from './CartIconStyles'
+import { useSelector } from 'react-redux'
+import { type RootState } from '../../../redux/store'
 
 const CartIcon = (): JSX.Element => {
+  const cart = useSelector((state: RootState) => state.cartState.cart)
+  const [totalCartAmount, setTotalCartAmount] = useState(0)
+
+  const calculateTotalCartItemsAmount = (): void => {
+    const totalAmount = cart.reduce((acc, { amount }) => acc + amount, 0)
+    setTotalCartAmount(totalAmount)
+  }
+
+  useEffect(() => {
+    calculateTotalCartItemsAmount()
+  }, [cart])
+
   return (
     <CartContainer>
-      <CartCounter>1</CartCounter>
+      <CartCounter>{totalCartAmount}</CartCounter>
       <BsFillCartCheckFill/>
     </CartContainer>
   )
