fc6bfad34e78c55eec3ed923870b81a8b156cd23
juliakocik <julia.kocik02@gmail.com>
2023-04-30 11:15:44 +0200

    Add Cart Counter styling

--- src/components/features/CartIcon/CartIcon.tsx
+++ src/components/features/CartIcon/CartIcon.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BsFillCartCheckFill } from 'react-icons/bs'
-import { CartContainer } from './CartIconStyles'
+import { CartContainer, CartCounter } from './CartIconStyles'
 
 const CartIcon = (): JSX.Element => {
   return (
     <CartContainer>
+      <CartCounter>1</CartCounter>
       <BsFillCartCheckFill/>
     </CartContainer>
   )
--- src/components/features/CartIcon/CartIconStyles.tsx
+++ src/components/features/CartIcon/CartIconStyles.tsx
@@ -8,4 +8,20 @@ export const CartContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
+`
+
+export const CartCounter = styled.span`
+    position: absolute;
+    bottom: 2rem;
+    font-size: 1rem;
+    right: 1.75rem;
+    color: ${colors.white};
+    background: ${colors.green};
+    border-radius: 50%;
+    height: 1.25rem;
+    width: 1.25rem;
+    text-align: center;
+    line-height: 1.25rem;
+
 `
