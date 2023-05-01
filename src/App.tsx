bc334e336fcabdff24b82c59728f16f068f1b5b0
juliakocik <julia.kocik02@gmail.com>
2023-05-01 21:17:58 +0200

    Add checkout to routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Category from './components/features/Categories/Category/Category'
 import NotFound from './components/common/NotFound/NotFound'
 import ProductDetails from './components/features/ProductDetails/ProductDetails'
 import CartScreen from './components/routes/CartScreen/CartScreen'
+import Checkout from './components/routes/Checkout/Checkout'
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: 'cart',
         element: <CartScreen />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />
       },
       {
         path: '/:category',
