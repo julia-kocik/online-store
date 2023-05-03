6d1f0b359046498de408b951f58789b4c2fd66ea
juliakocik <julia.kocik02@gmail.com>
2023-05-03 15:03:09 +0200

    Add template structure

--- src/components/routes/Checkout/Checkout.tsx
+++ src/components/routes/Checkout/Checkout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ButtonContainer, CheckoutContainer } from './CheckoutStyles'
+import { ButtonContainer, CheckoutContainer, CheckoutInnerContainer, CheckoutLeftContainer, CheckoutPersonalDetails, CheckoutSummaryContainer, CheckoutTotalPrice } from './CheckoutStyles'
 import Button from '../../common/Button/Button'
 import { colors } from '../../../colors'
 import { useSelector } from 'react-redux'
@@ -10,6 +10,13 @@ const Checkout = (): JSX.Element => {
   // const [isLoading, setIsLoading] = useState(false)
   return (
     <CheckoutContainer>
+      <CheckoutInnerContainer>
+          <CheckoutLeftContainer>
+            <CheckoutTotalPrice></CheckoutTotalPrice>
+            <CheckoutPersonalDetails></CheckoutPersonalDetails>
+          </CheckoutLeftContainer>
+          <CheckoutSummaryContainer></CheckoutSummaryContainer>
+      </CheckoutInnerContainer>
       {cart.length > 0 && (
         <ButtonContainer>
           <Button title='Process payment' color={colors.white} background={colors.green} fontSize='1rem' height='3rem'/>
--- src/components/routes/Checkout/CheckoutStyles.tsx
+++ src/components/routes/Checkout/CheckoutStyles.tsx
@@ -1,7 +1,31 @@
 import styled from 'styled-components'
 
 export const CheckoutContainer = styled.div`
-    
+    border: 3px solid red;
+    min-height: 80vh;
+`
+
+export const CheckoutInnerContainer = styled.div`
+    border: 3px solid orange;
+    min-height: 60vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`
+export const CheckoutLeftContainer = styled.div`
+    border: 3px solid blue;
+    display: grid;
+    grid-template-rows: 50%;
+`
+export const CheckoutTotalPrice = styled.div`
+    border: 3px solid yellow;
+`
+
+export const CheckoutPersonalDetails = styled.div`
+    border: 3px solid yellow;
+`
+
+export const CheckoutSummaryContainer = styled.div`
+    border: 3px solid green;
 `
 
 export const ButtonContainer = styled.div`
