431710748e95a8d610f0adfb7b4557b69b92ce95
juliakocik <julia.kocik02@gmail.com>
2023-05-03 14:42:52 +0200

    Add basic Checkout component

--- src/components/routes/Checkout/Checkout.tsx
+++ src/components/routes/Checkout/Checkout.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { ButtonContainer, CheckoutContainer } from './CheckoutStyles'
+import Button from '../../common/Button/Button'
+import { colors } from '../../../colors'
+import { useSelector } from 'react-redux'
+import { type RootState } from '../../../redux/store'
 
 const Checkout = (): JSX.Element => {
+  const cart = useSelector((state: RootState) => state.cartState.cart)
+  // const [isLoading, setIsLoading] = useState(false)
   return (
-    <div>Checkout</div>
+    <CheckoutContainer>
+      {cart.length > 0 && (
+        <ButtonContainer>
+          <Button title='Process payment' color={colors.white} background={colors.green} fontSize='1rem' height='3rem'/>
+        </ButtonContainer>
+      )}
+    </CheckoutContainer>
   )
 }
 
---
+++ src/components/routes/Checkout/CheckoutStyles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const CheckoutContainer = styled.div`
+    
+`
+
+export const ButtonContainer = styled.div`
+    display: grid;
+    justify-items: end;
+    width: 70%;
+    margin: 0 auto 3rem auto;
+`
