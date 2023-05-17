29a9bcf83151be3f4bad55098f9e3786d7f12bc3
juliakocik <julia.kocik02@gmail.com>
2023-05-17 21:59:52 +0200

    Add CartIcon and CartPreviewItems tests

--- src/components/features/CartIcon/CartIcon.test.tsx
+++ src/components/features/CartIcon/CartIcon.test.tsx
@@ -1,24 +1,17 @@
 import React from 'react'
-import { render } from '@testing-library/react'
 import CartIcon from './CartIcon'
-import { Provider } from 'react-redux/lib/alternate-renderers'
-import configureStore from 'redux-mock-store'
+import { renderWithProviders } from '../../../utils/test-utils'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('CartIcon component', () => {
   it('renders the cart counter with the correct length', () => {
-    const mockStore = configureStore()
-    const cartStore = mockStore({
-      cartState: {
-        cart: [{ amount: 2 }, { amount: 3 }, { amount: 1 }]
+    const { getByTestId } = renderWithProviders(<MemoryRouter><CartIcon /></MemoryRouter>, {
+      preloadedState: {
+        cartState: {
+          cart: [{ amount: 2 }, { amount: 3 }, { amount: 1 }]
+        }
       }
     })
-    const { getByTestId } = render(
-        <Provider store={cartStore}>
-          <MemoryRouter>
-            <CartIcon />
-          </MemoryRouter>
-        </Provider>)
     const cartCounter = getByTestId('cart-counter')
 
     expect(cartCounter).toBeInTheDocument()
---
+++ src/components/features/CartPreviewItems/CartPreviewItems.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { renderWithProviders } from '../../../utils/test-utils'
+import { MemoryRouter } from 'react-router-dom'
+import CartPreviewItems from './CartPreviewItems'
+import { fireEvent } from '@testing-library/react'
+
+describe('CartIcon component', () => {
+  it('renders cartAmount container with correctly increased value', () => {
+    const { getByTestId } = renderWithProviders(<MemoryRouter><CartPreviewItems /></MemoryRouter>, {
+      preloadedState: {
+        cartState: {
+          cart: [{ amount: 2 }]
+        }
+      }
+    })
+    const cartAmount = getByTestId('cart-amount')
+    const increaseBtn = getByTestId('cart-increase-button')
+    expect(cartAmount).toBeInTheDocument()
+    expect(cartAmount.textContent).toBe('2')
+
+    fireEvent.click(increaseBtn)
+    expect(cartAmount.textContent).toBe('3')
+  })
+
+  it('renders cartAmount container with correctly decreased value', () => {
+    const { getByTestId } = renderWithProviders(<MemoryRouter><CartPreviewItems /></MemoryRouter>, {
+      preloadedState: {
+        cartState: {
+          cart: [{ amount: 4 }]
+        }
+      }
+    })
+    const cartAmount = getByTestId('cart-amount')
+    const decreaseBtn = getByTestId('cart-decrease-button')
+    expect(cartAmount).toBeInTheDocument()
+    expect(cartAmount.textContent).toBe('4')
+
+    fireEvent.click(decreaseBtn)
+    expect(cartAmount.textContent).toBe('3')
+  })
+})
