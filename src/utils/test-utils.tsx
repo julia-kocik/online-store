1aec5d9c3e47038a46174d79237248519f3580d2
juliakocik <julia.kocik02@gmail.com>
2023-05-26 17:51:22 +0200

    Add test utils

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -4,6 +4,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 // As a basic setup, import your same slice reducers
 import { cartSlice } from '../../src/redux/cartSlice'
+import { productsSlice } from '../redux/productSlice'
 
 export function renderWithProviders (
   ui,
@@ -11,16 +12,18 @@ export function renderWithProviders (
     preloadedState = {},
     // Automatically create a store instance if no store was passed in
     store = configureStore({
-      reducer: { cartState: cartSlice.reducer },
+      reducer: { cartState: cartSlice.reducer, products: productsSlice.reducer },
       preloadedState
     }),
     ...renderOptions
   } = {}
 ): any {
   function Wrapper ({ children }): JSX.Element {
-    return <Provider store={store}>
-                    {children}
-            </Provider>
+    return (
+        <Provider store={store}>
+          {children}
+        </Provider>
+    )
   }
 
   // Return an object with the store and all of RTL's query functions
