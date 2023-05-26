2bc757b59afabcaec2b71b445fd1d38714264a7c
juliakocik <julia.kocik02@gmail.com>
2023-05-26 21:39:32 +0200

    Add secure localstorage and fix types for browser-storage

--- src/redux/browser-storage.ts
+++ src/redux/browser-storage.ts
@@ -1,15 +1,15 @@
 const KEY = 'redux'
-export function loadState (): any {
+export function loadState <T> (): T | undefined {
   try {
     const serializedState = localStorage.getItem(KEY)
     if (!serializedState) return undefined
     return JSON.parse(serializedState)
   } catch (e) {
-    return undefined
+    // Ignore
   }
 }
 
-export async function saveState (state: any): Promise<any> {
+export async function saveState <T> (state: T): Promise<void> {
   try {
     const serializedState = JSON.stringify(state)
     localStorage.setItem(KEY, serializedState)
