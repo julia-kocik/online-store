4b5fa30b126eceeec222d719a7a6e75cf217fa91
juliakocik <julia.kocik02@gmail.com>
2023-05-26 21:27:34 +0200

    Change a way how state is stored in localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { persistor, store } from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './redux/store'
+// import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { Elements } from '@stripe/react-stripe-js'
 import { stripePromise } from './stripe/stripe.utils'
 import GlobalStyles from './GlobalStyles'
 import Topbar from './components/features/TopBar/Topbar'
+import { saveState } from './redux/browser-storage'
+import { debounce } from 'debounce'
+
+store.subscribe(
+  // we use debounce to save the state once each 800ms
+  // for better performances in case multiple changes occur in a short time
+  debounce(() => {
+    saveState(store.getState())
+  }, 800)
+)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,13 +26,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
      <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <Elements stripe={stripePromise}>
           <Topbar/>
           <GlobalStyles />
           <App />
         </Elements>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,27 +1,29 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import productReducer from './productSlice'
 import cartReducer from './cartSlice'
-import storage from 'redux-persist/lib/storage'
-import { persistReducer, persistStore } from 'redux-persist'
+import { loadState } from './browser-storage'
+// import storage from 'redux-persist/lib/storage'
+// import { persistReducer, persistStore } from 'redux-persist'
 
-const persistConfig = {
-  key: 'root',
-  storage
-}
+// const persistConfig = {
+//   key: 'root',
+//   storage
+// }
 
 const rootReducer = combineReducers({
   products: productReducer,
   cartState: cartReducer
 })
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production'
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState: loadState()
 })
 
-export const persistor = persistStore(store)
+// export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
