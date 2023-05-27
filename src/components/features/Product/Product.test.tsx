fb2a60c20d32cbe87ddc9f2a5f71911a74dbfeb2
juliakocik <julia.kocik02@gmail.com>
2023-05-27 12:52:47 +0200

    Extract preloaded state to variable

--- src/components/features/Product/Product.test.tsx
+++ src/components/features/Product/Product.test.tsx
@@ -6,35 +6,37 @@ import { MemoryRouter } from 'react-router-dom'
 import { fireEvent } from '@testing-library/react'
 
 const productToAdd =
-    {
-      id: 3,
-      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-      price: 109.95,
-      description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-      category: 'mensclothing',
-      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-      rating: {
-        rate: 3.9,
-        count: 120
-      }
+  {
+    id: 3,
+    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price: 109.95,
+    description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    category: 'mensclothing',
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    rating: {
+      rate: 3.9,
+      count: 120
     }
+  }
+
+const preloadedState = {
+  cartState: {
+    cart: [{ id: 0, amount: 2 }, { id: 1, amount: 2 }]
+  },
+  products: {
+    data: {
+      electronics: testProducts
+    },
+    status: 'succeeded',
+    error: null,
+    searchTerm: ''
+  }
+}
 
 describe('CartIcon component', () => {
   it('renders product', async () => {
     const { findByRole, store } = renderWithProviders(<MemoryRouter><Product {...productToAdd}/></MemoryRouter>, {
-      preloadedState: {
-        cartState: {
-          cart: [{ id: 0, amount: 2 }, { id: 1, amount: 2 }]
-        },
-        products: {
-          data: {
-            electronics: testProducts
-          },
-          status: 'succeeded',
-          error: null,
-          searchTerm: ''
-        }
-      }
+      preloadedState
     })
 
     const addToCartBtn = await findByRole('button', {
